d2207af1b46b75485cdb3316c62755cd5e1fc1cb
Gustavo Allen <gustavoallen92@gmail.com>
2024-11-20 15:00:05 -0500

    started allpeople page styling

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -6,15 +6,44 @@ const PeoplePage: NextPage = async () => {
   const supabase = await createClient();
   const { data: people } = await supabase.from('People').select();
 
+  const peopleTemp: typeof people = [
+    {
+      created_at: '',
+      DOB: '10/07/1999',
+      fullName: 'John Doe',
+      id: 1,
+      occupations: ['Barista', 'Nurse', 'Something else'],
+      TOD: null,
+    },
+    {
+      created_at: '',
+      DOB: '08/12/1970',
+      fullName: 'Jane Dore',
+      id: 2,
+      occupations: ['Game Developer', 'Director'],
+      TOD: null,
+    },
+    {
+      created_at: '',
+      DOB: '12/03/1973',
+      fullName: 'Jannet Cole',
+      id: 3,
+      occupations: ['Event Planner'],
+      TOD: null,
+    },
+  ];
+
   // console.log(person);
 
   return (
     <section>
       <h1>people</h1>
       <pre>{JSON.stringify(people, null, 2)}</pre>
-      {people?.map((person) => {
-        return <SinglePerson person={person} key={person.id} />;
-      })}
+      <div className="flex flex-wrap gap-5 align-middle w-full">
+        {peopleTemp?.map((person) => {
+          return <SinglePerson person={person} key={person.id} />;
+        })}
+      </div>
     </section>
   );
 };
--- components/SinglePerson.tsx
+++ components/SinglePerson.tsx
@@ -7,8 +7,16 @@ interface SinglePersonProps {
 
 const SinglePerson: NextPage<SinglePersonProps> = async ({ person }) => {
   return (
-    <section>
-      <h1>{person?.fullName}</h1>
+    <section className="bg-slate-600 w-72 h-40 rounded-md">
+      <img
+        className="float-left w-24 h-24 mr-2 mb-2 rounded-tl-md"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmVq-OmHL5H_5P8b1k306pFddOe3049-il2A&s"
+      />
+      <div className="mt-1">
+        <h1>Name {person?.fullName}</h1>
+        <h2>DOB: {person?.DOB}</h2>
+        <div>Jobs: {person?.occupations?.join(' - ')}</div>
+      </div>
     </section>
   );
 };
